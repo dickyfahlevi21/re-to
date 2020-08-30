b2eeab6f5a64df9333f8418cd473cc67f2c16634
subhandp <subhan.dinda.putra@gmail.com>
2020-08-30 17:42:12 +0800

    add nodemailer functional when out of stock

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -1,46 +1,55 @@
 const nodemailer = require("nodemailer");
 
+
 class Email {
-  static emailTemplate(dataEmail) {
-    const html = /*html*/ `
+    static emailTemplate(dataEmail) {
+        const html = /*html*/ `
             <h1 style="text-align:center;padding:5px;border-radius:5px;background:#007bff;color:white">
-            Halo ${dataEmail.username}
+            Halo your product is empty. Details :
             </h1>
-            <p>Selamat datang</p>
+            <ul>
+                <li> Product name: ${dataEmail.productName}</li>
+            </ul>
          `;
-    return html;
-  }
+        return html;
+    }
 
-  static sendEmail(dataEmail, subject, filename, fileContent) {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+    static sendEmail(dataEmail, subject, filename, fileContent) {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            }
+        });
 
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: dataEmail.email,
-      subject: subject,
-      html: this.emailTemplate(dataEmail),
-      attachments: [
-        {
-          filename: filename,
-          content: fileContent,
-        },
-      ],
-    };
+        // const mailOptions = {
+        //     from: process.env.EMAIL_USERNAME,
+        //     to: dataEmail.email,
+        //     subject: subject,
+        //     html: this.emailTemplate(dataEmail),
+        //     attachments: [{
+        //         filename: filename,
+        //         content: fileContent
+        //     }]
+        // };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("Message sent: %s", info.messageId);
-      return;
-    });
-  }
+        const mailOptions = {
+            from: process.env.EMAIL_USERNAME,
+            to: process.env.EMAIL_USERNAME,
+            subject: subject,
+            html: Email.emailTemplate(dataEmail),
+        };
+
+        console.log('mail', mailOptions);
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            return;
+        });
+    }
 }
 
 module.exports = Email;
